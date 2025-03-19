7f88ad4bb03157cda806f10bddeddb40f6fae417
Luciano Lucena <luciano.lucena@mirgor.com.ar>
2025-03-19 17:39:32 -0300

    Agregamos Dockerfile y Nginx para Front

--- Tesla-Back/TeslaSpringBoot/src/main/java/com/Tesla/init/TeslaSpringBootApplication.java
+++ Tesla-Back/TeslaSpringBoot/src/main/java/com/Tesla/init/TeslaSpringBootApplication.java
@@ -2,12 +2,21 @@ package com.Tesla.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class TeslaSpringBootApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(TeslaSpringBootApplication.class, args);
+		// Iniciar la aplicación y obtener el entorno
+		Environment env = SpringApplication.run(TeslaSpringBootApplication.class, args).getEnvironment();
+
+		// Imprimir el puerto en los logs
+		System.out.println("🚀 Backend corriendo en el puerto: " + Objects.requireNonNull(env.getProperty("server.port")));
 	}
+
 
 }
