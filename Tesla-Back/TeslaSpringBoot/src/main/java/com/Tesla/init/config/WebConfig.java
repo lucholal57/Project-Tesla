5adcf3a5ca935af10103c54cb18d9ef37a06be43
Luciano Lucena <luciano.lucena@mirgor.com.ar>
2025-03-19 23:03:12 -0300

    Se modifica propertie para los cors backend

---
+++ Tesla-Back/TeslaSpringBoot/src/main/java/com/Tesla/init/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.Tesla.init.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permite cualquier origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Permitir credenciales
+    }
+}
